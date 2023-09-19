7c7d6771bc7236498d4a183963eee3c63fa05af9
babu97 <benkaimugul@gmail.com>
2023-09-19 06:04:04 +0300

    finisged variables section

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "babu"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+ let  excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+ let firstInitial = currentUser.charAt(0)
+
+ const shortGreeting = `Welcome, ${firstInitial}!`;
+ 
